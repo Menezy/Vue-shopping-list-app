e8bab36f48bf0ba39e2205652a8da0ddbda4b461
Brightmene <37218938+Brightmene@users.noreply.github.com>
2018-04-07 05:24:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta property="og:title" content=" vue shopping list application">
+    <meta property="og:type" content="app">
+    <meta property="og:description" content="A simple shopping list app with Vue.js and bootstrap 4">
     <link rel="stylesheet" href="b4/css/bootstrap.css">
     <link rel="stylesheet" href="styles/style.css">
     <title>Vue shopping list</title>
